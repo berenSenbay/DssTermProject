8058454eea496058e3bf345a506b6ffb47ad8b82
eylulseyit <eylulseyitoglu1@gmail.com>
2025-12-17 18:41:17 +0300

    libraries

--- csharp-ml-algorithm-comparator/Form1.Designer.cs
+++ csharp-ml-algorithm-comparator/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace csharp_ml_algorithm_comparator
+﻿using weka.core;
+
+namespace csharp_ml_algorithm_comparator
 {
     partial class Form1
     {
@@ -28,10 +30,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
